1b10dbe758c80d815197420f3c5744fa3afcd356
parsa gilpoor <amirparsagilpoor@gmail.com>
2024-06-21 21:55:04 +0330

    Add files via upload

---
+++ web.js
@@ -0,0 +1,43 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+function myFunction12() {
+    document.getElementById("dem").innerHTML="  برای راحتی و خرید اسان شما  در بهترین مکان و سریع ترین زمان ممکن برای شما  اماده ایم ";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
